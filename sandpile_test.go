84b5ecf6d7c1437e51a8c73646200d52e22d935b
Luke Sikina <luke@genomoncology.com>
2017-02-09 11:34:49 -0500

    Made unit test for grid position validation way nicer

--- sandpile_test.go
+++ sandpile_test.go
@@ -4,10 +4,27 @@ import (
 	"testing"
 )
 
+var gridPositionTestData = []struct {
+	xCoord, yCoord, xSize, ySize int
+	expected                     bool
+}{
+	{1, 1, 10, 10, true},
+	{0, 0, 10, 10, true},
+	{9, 9, 10, 10, true},
+	{-1, 0, 10, 10, false},
+	{0, -1, 10, 10, false},
+	{-1, -1, 10, 10, false},
+	{10, 0, 10, 10, false},
+	{10, 10, 10, 10, false},
+}
+
 func TestIsValidGridPosition(t *testing.T) {
-	coordinate := Coordinate{1, 1}
-	actual := coordinate.isValidGridPosition(1, 1)
-	if !actual {
-		t.Error("Expected true for a coordinate of 1, 1 in a 10x10 grid")
+	for _, scenario := range gridPositionTestData {
+		coordinate := Coordinate{scenario.xCoord, scenario.yCoord}
+		actual := coordinate.isValidGridPosition(scenario.xSize, scenario.ySize)
+		if actual != scenario.expected {
+			t.Errorf("Error for isValidGridPosition: for coordinate %d $d, in grid of size %dx%d, expected %b", scenario.xCoord, scenario.yCoord, scenario.xSize, scenario.ySize, scenario.expected)
+		}
+
 	}
 }
